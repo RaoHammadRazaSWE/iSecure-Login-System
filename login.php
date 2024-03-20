9d634d557d5747a6816d505ca4c1ebe43cde4bbf
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-03-20 12:44:27 +0500

    Create Login.php

---
+++ login.php
@@ -0,0 +1,90 @@
+<?php
+$login = false;
+$showError = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    include 'partials/_dbconnect.php';
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    // $sql = "Select * from users where username='$username' AND password='$password'";
+    $sql = "Select * from users where username='$username'";
+
+    $result = mysqli_query($conn, $sql);
+    $num = mysqli_num_rows($result);
+    if ($num == 1){
+        while($row=mysqli_fetch_assoc($result)){
+            if(password_verify($password, $row['password'])){
+                $login = true;
+                session_start();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $username;
+                header("location: welcome.php");
+            }
+            else{
+                $showError = "Invalid Credentials";
+            }
+        }
+        
+    }
+    else{
+        $showError = "Invalid Credentials";
+    }
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php require 'partials/_nav.php' ?>
+        <?php
+        if($login){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success!</strong> You are logged in.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            ';
+        }
+        if($showError){
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong>'.$showError.'
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            ';
+        }
+        ?>
+        <div class="container">
+            <h1 class="text-center">Login to our website</h1>
+            <form action="/loginsystem/login.php" method="post">
+            <div class="mb-3 col-md-6">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3 col-md-6">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary col-md-6">Login</button>
+            </form>
+
+        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
+</body>
+</html>
